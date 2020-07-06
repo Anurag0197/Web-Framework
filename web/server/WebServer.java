97e21d46237b5a564fc6f78733b81097a8243968
Anurag Kumar <anurag.kumar@geminisolutions.in>
2020-07-06 18:29:41 +0530

    Web Framework

--- web/server/WebServer.java
+++ web/server/WebServer.java
@@ -59,12 +59,5 @@ public class WebServer
         }
 
     }
-
-    public static void main(String[] args) throws IOException
-    {
-        File file = new File("/home/an.kumar1/Desktop/test/out/production/test/");
-        WebServer server = new WebServer(5009,file,"Abc");
-    }
-
 }
 
